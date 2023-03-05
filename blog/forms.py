ce3eb121d5fc938cb35e8f25a078140fe0301807
SergiyKochenko <sergiykochenko@gmail.com>
2023-03-05 19:59:04 +0000

    adjusting user interface

--- blog/forms.py
+++ blog/forms.py
@@ -9,3 +9,4 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
--- blog/models.py
+++ blog/models.py
@@ -47,5 +47,3 @@ class Comment(models.Model):
     def __str__(self):
         return f"Comment {self.body} by {self.name}"
 
-
-
